852f0899bc4d5fd931ffa8ed460037e8d64dafe5
villor <villor94@gmail.com>
2025-08-01 19:08:24 +0200

    Migrate tool_button to use EntityCursor

--- crates/bevy_feathers/src/controls/button.rs
+++ crates/bevy_feathers/src/controls/button.rs
@@ -100,8 +100,7 @@ pub fn tool_button(props: ButtonProps) -> impl Scene {
         template_value(props.variant)
         template_value(props.corners.to_border_radius(3.0))
         Hovered
-        // TODO: port CursonIcon to GetTemplate
-        // CursorIcon::System(bevy_window::SystemCursorIcon::Pointer)
+        EntityCursor::System(bevy_window::SystemCursorIcon::Pointer)
         TabIndex(0)
         ThemeBackgroundColor(tokens::BUTTON_BG)
         ThemeFontColor(tokens::BUTTON_TEXT)
